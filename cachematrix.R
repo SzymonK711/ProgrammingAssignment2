b1e6c300f6890497bbe17ac50c1f1b6fc3e0bcc3
SzymonK711 <szymon.k711@wp.pl>
2021-05-02 16:00:59 +0200

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,26 @@
-
+#Input a matrix that has to be cached in the environment
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
+  get <- function() x     
   setinvers <- function(invers) inv <<- invers
   getinvers <- function() inv
   list(set = set, get = get,
        setinvers = setinvers,
        getinvers = getinvers)
 }
-
-
-
+# Compute (if exist use previouse) inverse of matrix
 cacheSolve <- function(x, ...) {
   inv <- x$getinvers()
-  if(!is.null(inv)) {
+  if(!is.null(inv)) {    #Warn if inversed matrix exist
     message("getting cached data")
     return(inv)
   }
   mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setinvers(inv)
+  inv <- solve(mat, ...)  #inverse matrix
+  x$setinvers(inv)  
   inv
 }
